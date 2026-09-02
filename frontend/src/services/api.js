2756b9e95a8158622988754d1c289f8974bed3d6
Mahadevan <mahadevanshettiyar@gmail.com>
2026-09-02 16:28:51 +0530

    fix: hardcode backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://logicloop-backend.onrender.com/api",
 });
 
 // Attach token to every request automatically
